a2bb8dc8c18834a95b5ef7b469be6191494f7753
Abdallah Ali <abdallahalifci91295@gmail.com>
2019-09-12 06:26:05 +0200

    intial commit

---
+++ MVC in ios/MVC in ios/Controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC in ios
+//
+//  Created by Abdallah Ali on 9/12/19.
+//  Copyright © 2019 Abdallah Ali. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
